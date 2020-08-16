4b1469b9cfcc9288a2e4295e2b4ab8b8ce91de21
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-16 12:36:51 +0900

    Print star pattern

--- practice_the_codes.c
+++ practice_the_codes.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 int main(void) {
 
-	printf("Hello World");
+
+	int num1, i,j;
+	const int MAX = 9;
+
+	for (i = 0;i < MAX;i++) {
+		for (j = 0;j < i + 1;++j) {
+			printf("* ");
+		}
+		putchar('\n');
+	}
+
+
 	return 0;
 }
