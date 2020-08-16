94ef37519a3de25c8578c616c8664eb44bfe8eb4
Jason-time <noconal+jason-time-github@gmail.com>
2020-08-16 16:42:09 +0900

    Make sqrt() function

--- practice_the_codes.c
+++ practice_the_codes.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
-int main(void) {
+ #pragma warning(disable:4996)
 
-	int i = 0, j = 0;
+#include <stdio.h>
 
-	for(i=0;i<5;++i){
-		for (j = 0;j < 5 + i;++j) {
-			if (i + j >= 4)printf("*\t");
-			else
-			{
-				putchar('\t');
-			}
-		}
-		putchar('\n');
-	}
-	return 0;
+double sqrt(double a)
+{
+    double x = 2;
+
+    for (int i = 0; i < 10; i++)
+    {
+        x = (x + (a / x)) / 2;
+    }
+
+    return x;
 }
 
-/*==========================================*/
+void main()
+{
+    double a;
+
+    scanf("%lf",&a);
+
+    double result = sqrt(a);
+
+    printf("%.0f", result);
+
+}
+
+/*============= Make sqrt() function =============================*/
